1a58f5cdada01f0d4e02855f7bd7b02acc05f720
Scott Hoisington <hoyzington@gmail.com>
2020-09-03 00:06:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let driver = {};
 
 function updateDriverWithKeyAndValue(obj, key, value) {
-  return Object.assign({}, obj, { [key]: value });
+  return Object.assign({}, { [key]: value }, obj);
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
